f80f0dc3afee3af56ac4f8905bd3570639eb3a24
AccelAnalysis <jholman@accelanalysis.com>
2026-08-13 22:28:57 -0400

    Export deliverable lifecycle policy

--- packages/decision-output/src/index.ts
+++ packages/decision-output/src/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./ports.js";
 export * from "./domain/evidence-graph.js";
 export * from "./domain/policies.js";
+export * from "./domain/deliverable-policy.js";
 export * from "./service.js";
